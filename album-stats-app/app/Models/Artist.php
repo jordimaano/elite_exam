e21935c81909b8270688b531dde3b68b9717d17b
jordimaano <jordidimaano@gmail.com>
2025-10-24 09:49:36 +0800

    Created Artist Model, View, Controller

---
+++ album-stats-app/app/Models/Artist.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Artist extends Model
+{
+    protected $fillable = [
+        'code',
+        'name'
+    ];
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Register a callback to be executed before a new model is saved.
+        static::creating(function ($artist) {
+            // Find the maximum existing numeric suffix and increment it
+            $latestArtist = self::select(DB::raw('CAST(SUBSTR(code, 3) AS UNSIGNED) AS numeric_code'))
+                ->orderBy('numeric_code', 'desc')
+                ->first();
+
+            $newNumber = 1;
+            if ($latestArtist) {
+                $newNumber = $latestArtist->numeric_code + 1;
+            }
+
+            // Format the new code (e.g., A-1, A-2, A-10)
+            $artist->code = 'A-' . $newNumber;
+        });
+    }
+}
